f302509fd8c6aac4bf4e3a907160a0922ff3125d
zeeno77 <31478274+zeeno77@users.noreply.github.com>
2023-04-12 00:34:08 -0300

    Update main.blocks, main.ts, main.py

--- main.ts
+++ main.ts
@@ -11,20 +11,20 @@ let configTimeout = custom.setupESP(nerworkName, networkPassword, serverURL)
 basic.pause(basicPause)
 basic.clearScreen()
 basic.forever(function on_forever() {
-    let temperatura2: number;
-    let humedad2: number;
+    let temperatura: number;
+    let humedad: number;
     if (configTimeout < configTries) {
         dht11_dht22.queryData(DHTtype.DHT11, DigitalPin.P2, true, false, true)
         basic.pause(basicPause)
-        temperatura2 = dht11_dht22.readData(dataType.temperature)
-        if (temperatura2 != -999) {
-            custom.sendData("Casita", "Temperatura", "" + ("" + temperatura2))
+        temperatura = dht11_dht22.readData(dataType.temperature)
+        if (temperatura != -999) {
+            custom.sendData("Casita", "Temperatura", "" + ("" + temperatura))
         }
         
         basic.pause(basicPause)
-        humedad2 = dht11_dht22.readData(dataType.humidity)
-        if (humedad2 != -999) {
-            custom.sendData("Casita", "Humedad", "" + ("" + humedad2))
+        humedad = dht11_dht22.readData(dataType.humidity)
+        if (humedad != -999) {
+            custom.sendData("Casita", "Humedad", "" + ("" + humedad))
         }
         
         basic.pause(basicPause)
