be2fffec78540f61f0c0b0338fdf53a8bbcb94d0
zeeno77 <31478274+zeeno77@users.noreply.github.com>
2023-04-12 13:35:58 -0300

    Revert "Update pxt.json, main.blocks, main.ts, main.py"
    This reverts commit 3fbd466cf33281cb46beaadcb352727165bf0ec1.

--- main.ts
+++ main.ts
@@ -1,8 +1,49 @@
+let sensorGas = 0
+// humedad = 0
+// temperatura = 0
+let errorMsj = "Error"
 let basicPause = 5000
 let configTries = 10
-serial.redirectToUSB()
+let nerworkName = "arrakis"
+let networkPassword = "thespicemustflow"
+let serverURL = "http://192.168.88.53:8000/muestra/"
+let configTimeout = custom.setupESP(nerworkName, networkPassword, serverURL)
+basic.pause(basicPause)
 basic.clearScreen()
-basic.forever(function on_forever() {
-    serial.writeValue("x", pins.digitalReadPin(DigitalPin.P13))
-    basic.pause(basicPause)
+basic.forever(function () {
+    let temperatura: number;
+let humedad: number;
+if (configTimeout < configTries) {
+        dht11_dht22.queryData(
+        DHTtype.DHT11,
+        DigitalPin.P2,
+        true,
+        false,
+        true
+        )
+        basic.pause(basicPause)
+        temperatura = dht11_dht22.readData(dataType.temperature)
+        if (temperatura > -999) {
+            custom.sendData("Casita", "Temperatura", "" + temperatura)
+        }
+        basic.pause(basicPause)
+        humedad = dht11_dht22.readData(dataType.humidity)
+        if (humedad > -999) {
+            custom.sendData("Casita", "Humedad", "" + humedad)
+        }
+        basic.pause(basicPause)
+        if (pins.digitalReadPin(DigitalPin.P3) == 0) {
+            custom.sendData("Casita", "Gas", "Gas detectado")
+        }
+        basic.pause(basicPause)
+        custom.sendData("Casita", "Agua", "" + pins.analogReadPin(AnalogPin.P10))
+        basic.pause(basicPause)
+        if (pins.digitalReadPin(DigitalPin.P13) == 1) {
+            custom.sendData("Casita", "Movimiento", "Movimiento detectado")
+        }
+    } else {
+        serial.writeString(errorMsj)
+        basic.showString(errorMsj)
+        basic.pause(basicPause)
+    }
 })
