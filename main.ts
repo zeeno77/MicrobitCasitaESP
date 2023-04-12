06bf3ee67e7fe096ffd37ef0cfcb06e39259cf06
zeeno77 <31478274+zeeno77@users.noreply.github.com>
2023-04-11 23:45:42 -0300

    Update pxt.json, main.blocks, main.ts, main.py

--- main.ts
+++ main.ts
@@ -10,32 +10,31 @@ let serverURL = "http://192.168.88.53:8000/muestra/"
 let configTimeout = custom.setupESP(nerworkName, networkPassword, serverURL)
 basic.pause(basicPause)
 basic.clearScreen()
-basic.forever(function () {
-    // }
+basic.forever(function on_forever() {
+    
     if (configTimeout < configTries) {
-        // dht11_dht22.query_data(DHTtype.DHT11, DigitalPin.P2, True, False, True)
-        // basic.pause(basicPause)
-        // temperatura = dht11_dht22.read_data(dataType.TEMPERATURE)
-        // # if (temperatura != -999) {
-        // custom.send_data("Casita", "Temperatura", "" + str(temperatura))
-        // # }
-        // basic.pause(basicPause)
-        // humedad = dht11_dht22.read_data(dataType.HUMIDITY)
-        // # if (humedad != -999) {
-        // custom.send_data("Casita", "Humedad", "" + str(humedad))
-        // # }
+        //  dht11_dht22.query_data(DHTtype.DHT11, DigitalPin.P2, True, False, True)
+        //  basic.pause(basicPause)
+        //  temperatura = dht11_dht22.read_data(dataType.TEMPERATURE)
+        //  if (temperatura != -999) 
+        //  custom.send_data("Casita", "Temperatura", "" + str(temperatura))
+        //  basic.pause(basicPause)
+        //  humedad = dht11_dht22.read_data(dataType.HUMIDITY)
+        //  if (humedad != -999) 
+        //  custom.send_data("Casita", "Humedad", "" + str(humedad))
         basic.pause(basicPause)
         sensorGas = pins.digitalReadPin(DigitalPin.P3)
         if (sensorGas == 1) {
-            // if (humedad != -999) {
             custom.sendData("Casita", "Gas", "Gas detectado")
         } else {
-            // if (humedad != -999) {
             custom.sendData("Casita", "Gas", "Gas no detectado")
         }
+        
+        serial.writeString("" + ("" + pins.analogReadPin(AnalogPin.P10)))
     } else {
         serial.writeString(errorMsj)
         basic.showString(errorMsj)
         basic.pause(basicPause)
     }
+    
 })
