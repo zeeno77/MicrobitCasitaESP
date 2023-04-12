59da2c51f11ea0461db007f739e667ddd26face5
zeeno77 <31478274+zeeno77@users.noreply.github.com>
2023-04-11 23:52:34 -0300

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -7,31 +7,33 @@ configTries = 10
 nerworkName = "arrakis"
 networkPassword = "thespicemustflow"
 serverURL = "http://192.168.88.53:8000/muestra/"
-configTimeout = custom.setup_esp(nerworkName, networkPassword, serverURL)
+#configTimeout = custom.setup_esp(nerworkName, networkPassword, serverURL)
 basic.pause(basicPause)
 basic.clear_screen()
 
 def on_forever():
-    global sensorGas
-    if configTimeout < configTries:
-        # dht11_dht22.query_data(DHTtype.DHT11, DigitalPin.P2, True, False, True)
-        # basic.pause(basicPause)
-        # temperatura = dht11_dht22.read_data(dataType.TEMPERATURE)
-        # if (temperatura != -999) 
-        # custom.send_data("Casita", "Temperatura", "" + str(temperatura))
-        # basic.pause(basicPause)
-        # humedad = dht11_dht22.read_data(dataType.HUMIDITY)
-        # if (humedad != -999) 
-        # custom.send_data("Casita", "Humedad", "" + str(humedad))
-        basic.pause(basicPause)
-        sensorGas = pins.digital_read_pin(DigitalPin.P3)
-        if sensorGas == 1:
-            custom.send_data("Casita", "Gas", "Gas detectado")
-        else:
-            custom.send_data("Casita", "Gas", "Gas no detectado")
-        serial.write_string("" + str((pins.analog_read_pin(AnalogPin.P10))))
-    else:
-        serial.write_string(errorMsj)
-        basic.show_string(errorMsj)
-        basic.pause(basicPause)
+#    global sensorGas
+#    if configTimeout < configTries:
+#        # dht11_dht22.query_data(DHTtype.DHT11, DigitalPin.P2, True, False, True)
+#        # basic.pause(basicPause)
+#        # temperatura = dht11_dht22.read_data(dataType.TEMPERATURE)
+#        # if (temperatura != -999) 
+#        # custom.send_data("Casita", "Temperatura", "" + str(temperatura))
+#        # basic.pause(basicPause)
+#        # humedad = dht11_dht22.read_data(dataType.HUMIDITY)
+#        # if (humedad != -999) 
+#        # custom.send_data("Casita", "Humedad", "" + str(humedad))
+#        basic.pause(basicPause)
+#        sensorGas = pins.digital_read_pin(DigitalPin.P3)
+#        if sensorGas == 1:
+#            custom.send_data("Casita", "Gas", "Gas detectado")
+#        else:
+#            custom.send_data("Casita", "Gas", "Gas no detectado")
+#        
+#    else:
+#        serial.write_string(errorMsj)
+#        basic.show_string(errorMsj)
+#        basic.pause(basicPause)
+#    basic.pause(basicPause)
+    serial.write_string("" + str((pins.analog_read_pin(AnalogPin.P10))))    
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -7,34 +7,32 @@ let configTries = 10
 let nerworkName = "arrakis"
 let networkPassword = "thespicemustflow"
 let serverURL = "http://192.168.88.53:8000/muestra/"
-let configTimeout = custom.setupESP(nerworkName, networkPassword, serverURL)
+// configTimeout = custom.setup_esp(nerworkName, networkPassword, serverURL)
 basic.pause(basicPause)
 basic.clearScreen()
 basic.forever(function on_forever() {
-    
-    if (configTimeout < configTries) {
-        //  dht11_dht22.query_data(DHTtype.DHT11, DigitalPin.P2, True, False, True)
-        //  basic.pause(basicPause)
-        //  temperatura = dht11_dht22.read_data(dataType.TEMPERATURE)
-        //  if (temperatura != -999) 
-        //  custom.send_data("Casita", "Temperatura", "" + str(temperatura))
-        //  basic.pause(basicPause)
-        //  humedad = dht11_dht22.read_data(dataType.HUMIDITY)
-        //  if (humedad != -999) 
-        //  custom.send_data("Casita", "Humedad", "" + str(humedad))
-        basic.pause(basicPause)
-        sensorGas = pins.digitalReadPin(DigitalPin.P3)
-        if (sensorGas == 1) {
-            custom.sendData("Casita", "Gas", "Gas detectado")
-        } else {
-            custom.sendData("Casita", "Gas", "Gas no detectado")
-        }
-        
-        serial.writeString("" + ("" + pins.analogReadPin(AnalogPin.P10)))
-    } else {
-        serial.writeString(errorMsj)
-        basic.showString(errorMsj)
-        basic.pause(basicPause)
-    }
-    
+    //     global sensorGas
+    //     if configTimeout < configTries:
+    //         # dht11_dht22.query_data(DHTtype.DHT11, DigitalPin.P2, True, False, True)
+    //         # basic.pause(basicPause)
+    //         # temperatura = dht11_dht22.read_data(dataType.TEMPERATURE)
+    //         # if (temperatura != -999) 
+    //         # custom.send_data("Casita", "Temperatura", "" + str(temperatura))
+    //         # basic.pause(basicPause)
+    //         # humedad = dht11_dht22.read_data(dataType.HUMIDITY)
+    //         # if (humedad != -999) 
+    //         # custom.send_data("Casita", "Humedad", "" + str(humedad))
+    //         basic.pause(basicPause)
+    //         sensorGas = pins.digital_read_pin(DigitalPin.P3)
+    //         if sensorGas == 1:
+    //             custom.send_data("Casita", "Gas", "Gas detectado")
+    //         else:
+    //             custom.send_data("Casita", "Gas", "Gas no detectado")
+    //         
+    //     else:
+    //         serial.write_string(errorMsj)
+    //         basic.show_string(errorMsj)
+    //         basic.pause(basicPause)
+    //     basic.pause(basicPause)
+    serial.writeString("" + ("" + pins.analogReadPin(AnalogPin.P10)))
 })
